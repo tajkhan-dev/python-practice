42cbece5cc3e7bbfc083cc96de100ef5e2d9fd0a
Replit user <>
2023-10-26 07:46:48 +0000

    python project created

---
+++ python-datatypes-variables/main.py
@@ -0,0 +1,16 @@
+name="Ahmed"
+
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.find("med")) finds the index of the first occurrence of the string
+# print(name.split("m",1))
+# name=name.replace("Ahmed","hassaan")  
+# print(name)
+
+# print(name.isalpha())
+print(name.isdigit())
+# print(name.isalnum())
+# print(name.isspace())
+# print(name.islower())
+# print(name.isupper())
