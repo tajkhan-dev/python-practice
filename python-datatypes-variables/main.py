88ad0e70f015f8a37ea3f3d7948971956bce4ad6
tajkhan-dev <tajkhan.dev@gmail.com>
2023-12-05 18:17:08 +0000

    todo list with  python

--- python-datatypes-variables/main.py
+++ python-datatypes-variables/main.py
@@ -1,25 +1,37 @@
-name="Ahmed"
-#*** Strings Method ***
-# print(name.capitalize())
-# print(name.upper())
-# print(name.lower())
-# print(name.find("med")) finds the index of the first occurrence of the string
-# print(name.split("m",1))
-# name=name.replace("Ahmed","hassaan")  
-# print(name)
+# Initialize an empty list to store tasks
+myTodos = []
 
-# print(name.isalpha())
-# print(name.isdigit())
-# print(name.isalnum())
-# print(name.isspace())
-# print(name.islower())
-# print(name.isupper())
-#print(name.casefold())
+# Boolean variable to control the while loop
+check = True
 
+# Initialize a global variable 'info' with an empty string
+info = " "
 
+# Define a function to add tasks to the 'myTodos' list
+def addTodos():
+    # Declare 'info' as a global variable to modify it within the function
+    global info  
 
-#*** Integer methods ***
+    # Start an infinite loop controlled by the 'check' variable
+    while check:
+        # Check if the lowercase version of 'info' is "no"
+        if info.lower() == "no":
+            # Exit the loop if the user does not want to add more tasks
+            break
+        # Check if the lowercase version of 'info' is "yes"
+        elif info.lower() == "yes":
+            # Prompt the user to enter a task and add it to the list
+            todo = input("Enter a task to do today: ")
+            # Prompt the user to decide whether to add more items
+            info = input("Do you want to add more items? ")
+            # Append the entered task to the 'myTodos' list
+            myTodos.append(todo)
 
-num=10
+    # Return the list of tasks after the user is done
+    return myTodos
 
-print(float(num))
+# Call the 'addTodos' function and store the result in 'data'
+data = addTodos()
+
+# Print the final list of tasks
+print(data)
